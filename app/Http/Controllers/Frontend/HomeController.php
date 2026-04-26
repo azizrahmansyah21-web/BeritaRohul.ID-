79145757bda94288df905a908ce7180c752c938d
acengucop <telkomnyet889@gmail.com>
2026-04-26 23:26:24 +0700

    Menambahkan fitur settings

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -21,7 +21,6 @@ use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 
 class HomeController extends Controller
